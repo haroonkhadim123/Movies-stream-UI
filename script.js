ced3e0f04259048657c1c4d0faa90decaf3b5d75
Haroon Khadim <haroonkhadim971@gmail.com>
2025-06-17 15:42:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+let icon= document.querySelector('#icon');
+let para= document.querySelector('.question-paragraph');
+
+icon.addEventListener('click',()=>{
+    para.classList.toggle('question-paragraph1')
+    if(icon.classList.contains('fa-plus')){
+        icon.classList.remove('fa-plus')
+        icon.classList.add('fa-times')
+    }
+    else{
+        icon.classList.add('fa-plus')
+        icon.classList.remove('fa-times')
+
+
+    }
+})
+let icon1= document.querySelector('#icon1');
+let para1= document.querySelector('.question-paragraph2');
+
+icon1.addEventListener('click',()=>{
+    para1.classList.toggle('question-paragraph3')
+    if(icon1.classList.contains('fa-plus')){
+        icon1.classList.remove('fa-plus')
+        icon1.classList.add('fa-times')
+    }
+    else{
+        icon1.classList.add('fa-plus')
+        icon1.classList.remove('fa-times')
+
+
+    }
+})
+let icon2= document.querySelector('#icon2');
+let para2= document.querySelector('.question-paragraph4');
+
+icon2.addEventListener('click',()=>{
+    para2.classList.toggle('question-paragraph5')
+    if(icon2.classList.contains('fa-plus')){
+        icon2.classList.remove('fa-plus')
+        icon2.classList.add('fa-times')
+    }
+    else{
+        icon2.classList.add('fa-plus')
+        icon2.classList.remove('fa-times')
+
+
+    }
+})
+let icon3= document.querySelector('#icon3');
+let para3= document.querySelector('.question-paragraph6');
+
+icon3.addEventListener('click',()=>{
+    para3.classList.toggle('question-paragraph7')
+    if(icon3.classList.contains('fa-plus')){
+        icon3.classList.remove('fa-plus')
+        icon3.classList.add('fa-times')
+    }
+    else{
+        icon3.classList.add('fa-plus')
+        icon3.classList.remove('fa-times')
+
+
+    }
+})
+let icon4= document.querySelector('#icon4');
+let para4= document.querySelector('.question-paragraph8');
+
+icon4.addEventListener('click',()=>{
+    para4.classList.toggle('question-paragraph9')
+    if(icon4.classList.contains('fa-plus')){
+        icon4.classList.remove('fa-plus')
+        icon4.classList.add('fa-times')
+    }
+    else{
+        icon4.classList.add('fa-plus')
+        icon4.classList.remove('fa-times')
+
+
+    }
+})
+let icon5= document.querySelector('#icon5');
+let para5= document.querySelector('.question-paragraph10');
+
+icon5.addEventListener('click',()=>{
+    para5.classList.toggle('question-paragraph11')
+    if(icon5.classList.contains('fa-plus')){
+        icon5.classList.remove('fa-plus')
+        icon5.classList.add('fa-times')
+    }
+    else{
+        icon5.classList.add('fa-plus')
+        icon5.classList.remove('fa-times')
+
+
+    }
+})
